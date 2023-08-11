2290cd2ff3d818b2cebb3a40c2c953d7db2b85dd
Danusha <danush99online@gmail.com>
2023-08-11 16:12:51 +0530

    add toastify

---
+++ Server/frontend/src/helpers/ToasterMessage.js
@@ -0,0 +1,45 @@
+import { toast } from "react-toastify";
+
+const ErrorMessage = ({
+    error,
+    main_part = 'REQUEST FAILED',
+    default_message = 'UNKNOWN ERROR OCCOURED',
+    custom_message = ''
+}) => {
+
+    // console.log("error response message : ", error?.response?.data?.message);
+    // console.log("error response :", error?.response);
+
+    let message = error?.response?.data?.message[0].message ?? error?.response?.data?.message;
+    message = message ? message : main_part + " " + default_message;
+
+    message = custom_message ? custom_message : message;
+
+    toast.error(message, {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    });
+}
+
+const SuccessMessage = (message = 'REQUEST SUCCESSFUL',) => {
+
+    toast.success(message, {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    });
+}
+
+export default {
+    ErrorMessage,
+    SuccessMessage
+};
